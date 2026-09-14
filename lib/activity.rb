f15f84f81e9e9ea3522c488a3c7dd039cdd6bf77
Eugene <eugeneku123@gmail.com>
2026-09-13 19:40:03 -0500

    refactor: ran rubocop -A and converted variable names into snake_case

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,49 +1,39 @@
 # frozen_string_literal: true
 
 class Activity
+  def initialize(name, metric_value)
+    @sets = []
+    @name = name
+    @metric_value = metric_value
+    @set_count = 0
+  end
 
-    def initialize(name, metricValue)
-        @sets = []
-        @name = name
-        @metricValue = metricValue
-        @setCount = 0
+  attr_reader :name, :metric_value
+
+  def edit_name(new_name)
+    @name = new_name
+  end
+
+  def add_set(set)
+    @sets << set
+    @set_count += 1
+  end
+
+  def view_sets
+    @sets.each do |set|
+      puts set
     end
+  end
 
-    def name()
-        @name
-    end
-
-    def edit_name(new_name)
-        @name = new_name
-    end
-
-    def add_set(set)
-        @sets << set
-        @setCount += 1
-    end
-
-    def view_sets()
-        @sets.each do |set|
-            puts set
-        end
-    end
-
-    def metricValue()
-        @metricValue
-    end
-
-    def edit_metricValue(new_metricValue)
-        @metricValue = new_metricValue
-    end
-
+  def edit_metric_value(new_metric_value)
+    @metric_value = new_metric_value
+  end
 end
 
 class Set
-
-    def initialize(weight, reps, rpe)
-        @weight = weight
-        @reps = reps
-        @rpe = rpe
-    end
-
+  def initialize(weight, reps, rpe)
+    @weight = weight
+    @reps = reps
+    @rpe = rpe
+  end
 end
--- lib/session.rb
+++ lib/session.rb
@@ -1,33 +1,27 @@
+# frozen_string_literal: true
+
 class Session
+  def initialize(name)
+    @name = name
+    @activity_count = 0
+    @date = Time.now
+    @activities = []
+  end
 
-    def initialize(name)
-        @name = name
-        @activityCount = 0
-        @date = Time.now
-        @activities = []
+  attr_reader :name, :date
+
+  def edit_name(new_name)
+    @name = new_name
+  end
+
+  def add_activity(activity)
+    @activities << activity
+    @activity_count += 1
+  end
+
+  def view_activities
+    @activities.each do |activity|
+      puts activity
     end
-
-    def name()
-        @name
-    end
-
-    def edit_name(new_name)
-        @name = new_name
-    end
-
-    def add_activity(activity)
-        @activities << activity
-        @activityCount += 1
-    end
-
-    def view_activities()
-        @activities.each do |activity|
-            puts activity
-        end
-    end
-
-    def date()
-        @date
-    end
-
+  end
 end
